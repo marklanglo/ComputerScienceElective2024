e582d4f202087edcb3a4cdf964d9cfbe604415ef
naveenbaha <bahadur.naveen@gmail.com>
2023-12-12 08:20:21 -0800

    Add files via upload

---
+++ Computer Science Elective 2024/Teacher/Graphics Project/Picture.java
@@ -0,0 +1,124 @@
+public class Picture
+{
+    public static void main(String[] args)
+    {
+        Picture p = new Picture();
+        p.draw();
+    }
+
+    /**
+     * Draw this picture.  The colors available are: red, black, blue, yellow, green,
+     * magenta, white, gray, orange, pink, dark gray, light gray, and cyan.  
+     * In the changeColor method, supply the color as a String in all lowercase as
+     * written above.
+     * 
+     * A basic Circle goes like this:
+     * Circle sun = new Circle();
+     * sun.changeColor("yellow");
+     * sun.moveHorizontal(240);
+     * sun.moveVertical(50);
+     * sun.changeSize(60);
+     * sun.makeVisible();
+     * 
+     */
+    public void draw()
+    {
+        Rect background = new Rect();
+        background.moveVertical(-100);
+        background.moveHorizontal(-100);
+        background.changeSize(1000000, 1000000);
+        background.changeColor("green");
+        background.makeVisible();
+        Rect playingCard = new Rect();
+        playingCard.moveVertical(60);
+        playingCard.moveHorizontal(10);
+        playingCard.changeSize(250,180);
+        playingCard.changeColor("white");
+        playingCard.makeVisible();
+        Triangle diamond = new Triangle();
+        diamond.moveVertical(200);
+        diamond.moveHorizontal(200);
+        diamond.changeSize(-20, 30);
+        diamond.changeColor("red");
+        diamond.makeVisible();
+        Triangle diamond2 = new Triangle();
+        diamond2.moveVertical(160);
+        diamond2.moveHorizontal(200);
+        diamond2.changeSize(20, 30);
+        diamond2.changeColor("red");
+        diamond2.makeVisible();
+        Triangle bigDiamond = new Triangle();
+        bigDiamond.moveVertical(200);
+        bigDiamond.moveHorizontal(160);
+        bigDiamond.changeSize(40, 50);
+        bigDiamond.changeColor("red");
+        bigDiamond.makeVisible();
+        Triangle bigDiamond2 = new Triangle();
+        bigDiamond2.moveVertical(280);
+        bigDiamond2.moveHorizontal(160);
+        bigDiamond2.changeSize(-40, 50);
+        bigDiamond2.changeColor("red");
+        bigDiamond2.makeVisible();
+        Triangle otherDiamond = new Triangle();
+        otherDiamond.moveVertical(315);
+        otherDiamond.moveHorizontal(110);
+        otherDiamond.changeSize(-20, 30);
+        otherDiamond.changeColor("red");
+        otherDiamond.makeVisible();
+        Triangle otherDiamond2 = new Triangle();
+        otherDiamond2.moveVertical(275);
+        otherDiamond2.moveHorizontal(110);
+        otherDiamond2.changeSize(20, 30);
+        otherDiamond2.changeColor("red");
+        otherDiamond2.makeVisible();
+        Triangle A = new Triangle();
+        A.moveVertical(120);
+        A.moveHorizontal(100);
+        A.changeSize(50, 40);
+        A.changeColor("red");
+        A.makeVisible();
+        Triangle A0 = new Triangle();
+        A0.moveVertical(120);
+        A0.moveHorizontal(100);
+        A0.changeSize(10, 20);
+        A0.changeColor("white");
+        A0.makeVisible();
+        Triangle A2= new Triangle();
+        A2.moveVertical(140);
+        A2.moveHorizontal(100);
+        A2.changeSize(30, 20);
+        A2.changeColor("white");
+        A2.makeVisible();
+        Rect AL= new Rect();
+        AL.moveVertical(100);
+        AL.moveHorizontal(30);
+        AL.changeColor("red");
+        AL.changeSize(5, 20);
+        AL.makeVisible();
+        
+        Triangle Ab = new Triangle();
+        Ab.moveVertical(350);
+        Ab.moveHorizontal(220);
+        Ab.changeSize(-50, 40);
+        Ab.changeColor("red");
+        Ab.makeVisible();
+        Triangle A0b = new Triangle();
+        A0b.moveVertical(350);
+        A0b.moveHorizontal(220);
+        A0b.changeSize(-10, 20);
+        A0b.changeColor("white");
+        A0b.makeVisible();
+        Triangle A2b= new Triangle();
+        A2b.moveVertical(330);
+        A2b.moveHorizontal(220);
+        A2b.changeSize(-30, 20);
+        A2b.changeColor("white");
+        A2b.makeVisible();
+        Rect AL2= new Rect();
+        AL2.moveVertical(267);
+        AL2.moveHorizontal(152);
+        AL2.changeColor("red");
+        AL2.changeSize(5, 20);
+        AL2.makeVisible();
+            }
+}
---
+++ Computer Science Elective 2024/Teacher/Graphics Project/Square.java
@@ -0,0 +1,202 @@
+import java.awt.*;
+
+/**
+ * A square that can be manipulated and that draws itself on a canvas.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 1.0  (15 July 2000)
+ */
+
+public class Square
+{
+    private int size;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private boolean isVisible;
+
+    /**
+     * Create a new square at default position with default color.
+     */
+    public Square()
+    {
+        size = 30;
+        xPosition = 0;
+        yPosition = 0;
+        color = "red";
+        isVisible = false;
+    }
+
+    /**
+     * Make this square visible. If it was already visible, do nothing.
+     */
+    public void makeVisible()
+    {
+        isVisible = true;
+        draw();
+    }
+
+    /**
+     * Make this square invisible. If it was already invisible, do nothing.
+     */
+    public void makeInvisible()
+    {
+        erase();
+        isVisible = false;
+    }
+
+    /**
+     * Move the square a few pixels to the right.
+     */
+    public void moveRight()
+    {
+        moveHorizontal(20);
+    }
+
+    /**
+     * Move the square a few pixels to the left.
+     */
+    public void moveLeft()
+    {
+        moveHorizontal(-20);
+    }
+
+    /**
+     * Move the square a few pixels up.
+     */
+    public void moveUp()
+    {
+        moveVertical(-20);
+    }
+
+    /**
+     * Move the square a few pixels down.
+     */
+    public void moveDown()
+    {
+        moveVertical(20);
+    }
+
+    /**
+     * Move the square horizontally by 'distance' pixels.
+     */
+    public void moveHorizontal(int distance)
+    {
+        erase();
+        xPosition += distance;
+        draw();
+    }
+
+    /**
+     * Move the square vertically by 'distance' pixels.
+     */
+    public void moveVertical(int distance)
+    {
+        erase();
+        yPosition += distance;
+        draw();
+    }
+
+    /**
+     * Slowly move the square horizontally by 'distance' pixels.
+     */
+    public void slowMoveHorizontal(int distance, int multiplier)
+    {
+        int delta;
+        
+        if(distance < 0) 
+        {
+            delta = -1 * multiplier;
+            distance = -distance;
+        }
+        else 
+        {
+            delta = 1 * multiplier;
+        }
+
+        for(int i = 0; i < distance; i+= multiplier)
+        {
+            xPosition += delta;
+            draw();
+            try {
+            Thread.sleep(1);
+            } catch(Exception e) {
+            System.out.println("This didn't work");
+        }
+        }
+    }
+
+    /**
+     * Slowly move the square vertically by 'distance' pixels.
+     */
+    public void slowMoveVertical(int distance, int multiplier)
+    {
+        int delta;
+
+        if(distance < 0) 
+        {
+            delta = -1 * multiplier;
+            distance = -distance;
+        }
+        else 
+        {
+            delta = 1 * multiplier;
+        }
+
+        for(int i = 0; i < distance; i+= multiplier)
+        {
+            yPosition += delta;
+            draw();
+            try {
+            Thread.sleep(1);
+            } catch(Exception e) {
+            System.out.println("This didn't work");
+        }
+        }
+    }
+
+    /**
+     * Change the size to the new size (in pixels). Size must be >= 0.
+     */
+    public void changeSize(int newSize)
+    {
+        erase();
+        size = newSize;
+        draw();
+    }
+
+    /**
+     * Change the color. Valid colors are "red", "yellow", "blue", "green",
+     * "magenta", "black", "gray", "orange", "pink", "dark gray", "white",
+     * "light gray", and "cyan".
+     */
+    public void changeColor(String newColor)
+    {
+        color = newColor;
+        draw();
+    }
+
+    /*
+     * Draw the square with current specifications on screen.
+     */
+    private void draw()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.draw(this, color,
+                    new Rectangle(xPosition, yPosition, size, size));
+            canvas.wait(10);
+        }
+    }
+
+    /*
+     * Erase the square on screen.
+     */
+    private void erase()
+    {
+        if(isVisible) {
+            Canvas canvas = Canvas.getCanvas();
+            canvas.erase(this);
+        }
+    }
+}
